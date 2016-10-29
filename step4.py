949f0f354f61420200d52b5049940f143a8d4fa4
nasifibn <nasifmajeed@gmail.com>
2016-10-29 19:36:44 -0400

    Update step4.py

--- step4.py
+++ step4.py
@@ -2,7 +2,7 @@ import requests
 
 import json
 
-token = '83d9b617120d0299bc0e0c3ef0397752'
+token = '8e9b4ca6f16475b8662f51c070d4403d'
 firstEndpoint = 'http://challenge.code2040.org/api/prefix'
 secondEndpoint = 'http://challenge.code2040.org/api/prefix/validate'
 
